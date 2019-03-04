4327bee1d5a2b12a37e5428ce1444dbdae2f0e0b
Vladislav Lisetskii <vladislav_lisetskii@epam.com>
2019-03-04 15:50:27 +0300

    fix: simplify solution using Map

--- src/index.js
+++ src/index.js
@@ -1,23 +1,14 @@
 module.exports = function check(str, bracketsConfig) {
+  const map = new Map(bracketsConfig);
   var stack = [];
   for (let i = 0; i < str.length; i++) {
     const currentElement = str[i];
     const previousElement = stack[stack.length - 1];
-    if (configHasRule(previousElement, currentElement, bracketsConfig)) {
+    if (map.get(previousElement) === currentElement) {
       stack.pop();
     } else {
       stack.push(currentElement);
     }
   }
   return stack.length == 0;
-}
-
-function configHasRule(openingBracket, closingBracket, bracketsConfig) {
-  for (let i = 0; i < bracketsConfig.length; i++) {
-    const element = bracketsConfig[i];
-    if (element[0] == openingBracket && element[1] == closingBracket) {
-      return true;
-    }
-  }
-  return false;
 }
