bf2bb8af43fec1ee9aa08be84bbd9512f87832e8
Vladislav Lisetskii <vladislav_lisetskii@epam.com>
2019-03-04 15:14:01 +0300

    feat: implement brackets check

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  var stack = [];
+  for (let i = 0; i < str.length; i++) {
+    const currentElement = str[i];
+    const previousElement = stack[stack.length - 1];
+    if (configHasRule(previousElement, currentElement, bracketsConfig)) {
+      stack.pop();
+    } else {
+      stack.push(currentElement);
+    }
+  }
+  return stack.length == 0;
+}
+
+function configHasRule(openingBracket, closingBracket, bracketsConfig) {
+  for (let i = 0; i < bracketsConfig.length; i++) {
+    const element = bracketsConfig[i];
+    if (element[0] == openingBracket && element[1] == closingBracket) {
+      return true;
+    }
+  }
+  return false;
 }
